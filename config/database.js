4a7e7be3d8d808369f5fd68eeb51d4a14bbeda91
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-07-17 10:51:57 -0500

    config database develop

--- config/database.js
+++ config/database.js
@@ -28,11 +28,16 @@ module.exports = ({ env }) => {
         default: {
           connector: 'bookshelf',
           settings: {
-            client: 'sqlite',
-            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+            client: 'postgres',
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            username: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: env.bool('DATABASE_SSL'),
           },
           options: {
-            useNullAsDefault: true,
+            ssl: false,
           },
         },
       },
