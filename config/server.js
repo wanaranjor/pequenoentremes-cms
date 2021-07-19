40141f09714f9912304eeeb888cbf8bbd23fd1a0
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-07-19 14:27:34 -0500

    [update] server admi url

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('URL_API'),
   admin: {
+    url: env('URL_API_ADMIN'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cf1bcdd7e2a68cd9adf2ae5f2807a5fd'),
     },
