5d296a61565c0fc18af6b426cd45eab0a89078a8
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-07-19 14:17:01 -0500

    [update] config url server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST'),
-  port: env.int('PORT'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('URL_API'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cf1bcdd7e2a68cd9adf2ae5f2807a5fd'),
